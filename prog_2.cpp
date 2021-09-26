ec28990e0ee59ff6b08812d67342554520f3a949
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-09-26 19:25:02 +0300

    chore: even more lines, same result

--- prog_2.cpp
+++ prog_2.cpp
@@ -10,6 +10,13 @@ int main()
     std::cin >> v1;
     std::cin >> v2;
 
-    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+    std::cout << "The sum of ";
+    std::cout << v1;
+    std::cout << " and ";
+    std::cout << v2;
+    std::cout << " is ";
+    std::cout << v1 + v2;
+    std::cout << std::endl;
+
     return 0;
 }
