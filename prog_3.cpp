62a2058883a1996e1ab6e622f015a7830af7b089
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-09-26 19:55:22 +0300

    feat: calculate value of the input range

--- prog_3.cpp
+++ prog_3.cpp
@@ -3,14 +3,24 @@
 int main()
 {
     int sum = 0;
-    int val = 1;
 
-    while (val <= 10) {
-        sum += val;
-        ++val;
+    int range_start = 1;
+    int range_end = 10;
+
+    std::cout << "Enter two numbers:";
+    std::cout << std::endl;
+
+    std::cin >> range_start;
+    std::cin >> range_end;
+
+    int current_value = range_start;
+
+    while (current_value <= range_end) {
+        sum += current_value;
+        ++current_value;
     }
 
-    std::cout << " Sum of 1 to 10 inclusive is ";
+    std::cout << " Sum of " << range_start << " to " << range_end << " inclusive is ";
     std::cout << sum;
     std::cout << std::endl;
 
